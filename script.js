6cfcb29ad71ba5ca7cd51158f453c2bf2f860ec6
sabir546 <mdgulamsabir9@gmail.com>
2023-09-13 00:36:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+var tl= gsap.timeline()
+tl.to("#page1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0,
+})
+tl.to("#page1",{
+    y:"0vh",
+delay:1,
+    duration:1,
+})
+
+tl.to("#page1",{
+    y:"0vh",
+
+    rotate:-720,
+scale:1,
+duration:0.8,
+    
+})
